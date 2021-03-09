546f53378accc47af5d3ad82056e99caa3ca3e31
Sivaparvathi Ch <Sivaparvathi Ch@SivaparvathiCh>
2021-03-09 15:23:30 +0530

    siva nbfdsnb

---
+++ orangehrm1234/src/main/java/orangehrm1234/orangehrm1234/App.java
@@ -0,0 +1,13 @@
+package orangehrm1234.orangehrm1234;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
